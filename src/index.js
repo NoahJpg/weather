63f02df83c8a4791808102ddd648035ecea24341
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-02-07 10:09:52 -0800

    update project to use promise, separate logic, and make static method

--- src/index.js
+++ src/index.js
@@ -1,40 +1,33 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
-// Business Logic
-
-function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    }
-    request.addEventListener("readystatechange", function() {
-      console.log(this.readyState);
-    });
-  });
-
-  request.open("GET", url, true);
-  request.send();
-}
+import WeatherService from './weather-service.js';
 
 // UI Logic
-
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
-}
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const city = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
-  getWeather(city);
+  // return promise - alternate orginzation 
+  let promise = WeatherService.getWeather(city);
+  promise.then(function(weatherDataArray) { 
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
 }
+
+function printElements(data) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${data[1]} is ${data[0].main.humidity}%.
+  The temperature in Kelvins is ${data[0].main.temp} degrees.`;
+}
+
+function printError(error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+}
+
+
 
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
